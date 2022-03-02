bd0299890d9b2d717af81e9add7471732a5943b9
Prajwal Jaiswal <prajwalmjaiswal@gmail.com>
2022-03-02 19:36:10 +0530

    Create CRUD app using MERN Stack

---
+++ products_module.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const Db = process.env.ATLAS_URI;
+
+// Step 1 - Connect to database
+const connString = Db;
+mongoose
+  .connect(connString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Database Connected"))
+  .catch((error) => console.log(error));
+
+// Step 2 - Create Schema
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: String,
+    required: true,
+  },
+});
+
+// Step 3 - Create guests Collection model
+const productObject = new mongoose.model("products", productSchema);
+
+exports.Product = productObject;
---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express();
+require('dotenv').config();
+
+const productModel = require("./products_module");
+const product = productModel.Product;
+
+const PORT = 5000;
+
+// add express.json middleware
+app.use(express.json());
+
+var cors = require("cors");
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello Friends!");
+});
+
+app.get("/products", async (req, res) => {
+  let data = await product.find();
+  res.send(data);
+});
+
+app.get("/products/:id", async (req, res) => {
+  console.log(req.params.id);
+  let data = await product.find({ _id: req.params.id });
+  res.send(data[0]);
+});
+
+app.post("/products/add", async (req, res) => {
+  console.log(req.body);
+  let entry = await product(req.body);
+  let result = entry.save();
+  console.log("[Data Added]");
+  res.send(result);
+});
+
+app.post("/update/:id", async (req, res) => {
+  console.log(req.body);
+  let update_data = await product.updateOne(
+    { _id: req.params.id },
+    {
+      $set: {
+        name: req.body.name,
+        description: req.body.description,
+        rating: req.body.rating,
+        price: req.body.price,
+      },
+    }
+  );
+  console.log("[Data Updated]");
+  res.send(update_data);
+});
+
+app.delete("/:id", async (req, res) => {
+  let delete_data = await product.deleteOne({ _id: req.params.id  });
+  console.log("[Data Deleted]");
+  res.send(delete_data);
+});
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
